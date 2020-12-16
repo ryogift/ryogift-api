db9c47256b36ebe98027d20ef2a1ba6116d4e588
Ryo Takahashi <info.ry0takahash1@gmail.com>
2020-12-17 03:28:06 +0900

    クライアント側のポート変更に合わせて、cors設定を修正

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,7 @@ module RyogiftApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "http://localhost:3000"
+        origins "http://localhost:3001"
         resource "*",
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
