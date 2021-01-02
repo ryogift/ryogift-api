4f15255a650625f13302b0aeaf476d1c4b8cba8c
Ryo Takahashi <ryogiftjp@gmail.com>
2021-01-03 03:39:55 +0900

    specフォルダをコードカバレッジ対象から除外

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,7 @@ require "simplecov"
 
 SimpleCov.profiles.define "myprofile" do
   add_filter "/config/"
+  add_filter "/spec/"
 
   add_group "Controllers", "app/controllers"
   add_group "Models", "app/models"
