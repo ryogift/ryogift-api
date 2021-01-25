7b56175faf9097ea12173b522d187b1c6f7afa24
Ryo Takahashi <info.ry0takahash1@gmail.com>
2021-01-26 01:00:29 +0900

    travis用に修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-email = ENV.fetch("ADMIN_EMAIL")
-password_digest = User.digest(ENV.fetch("ADMIN_PASSWORD"))
-User.create(name: "管理者", email: email, password_digest: password_digest,
-            admin: true, state: :active, activated_at: Time.zone.now)
+# email = ENV.fetch("ADMIN_EMAIL")
+# password_digest = User.digest(ENV.fetch("ADMIN_PASSWORD"))
+# User.create(name: "管理者", email: email, password_digest: password_digest,
+#             admin: true, state: :active, activated_at: Time.zone.now)
