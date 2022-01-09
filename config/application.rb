b90198d11bef0d3f7aff3b60b3d3034e355bc30d
Ryo Takahashi <ryogiftjp@gmail.com>
2022-01-09 16:19:12 +0900

    Fix config.load_defaults 7.0

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module RyogiftApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
     config.i18n.default_locale = :ja
     config.time_zone = "Asia/Tokyo"
     config.active_record.default_timezone = :local
